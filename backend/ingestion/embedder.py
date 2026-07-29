bed2f30de9e378d1df26ce9f42899451613527dc
suhas859 <19pa1a05f4@vishnu.edu.in>
2026-07-29 11:38:38 +0200

    added embed_query()

--- backend/ingestion/embedder.py
+++ backend/ingestion/embedder.py
@@ -20,3 +20,16 @@ def embed_chunks(chunks: list[str]):
 
     return embeddings.tolist()
 
+
+def embed_query(question: str):
+    
+    formatted_query = f"search_query: {question}"
+
+    embedding = model.encode(
+        formatted_query,
+        normalize_embeddings=True,
+        convert_to_numpy=True
+    )
+
+    return embedding.tolist()
+
